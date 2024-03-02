ba65e57113f8cdf873467b3b444d3b41f028c841
Taiywo <54299107+Taiywo@users.noreply.github.com>
2024-03-02 08:58:09 +0100

    style  button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -9,7 +9,7 @@ const onClicked = () => {
 };
   return (
     <button
-    className={`bg-blue-800 hover:bg-blue-950 text-white font-bold py-2 px-4 rounded`}
+    className={`bg-blue-800 hover:bg-blue-950 text-white font-bold py-4 px-6 rounded`}
     onClick={onClicked}
     
   >
