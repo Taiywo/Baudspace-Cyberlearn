b216444777a1777995611a12b51ea96b29572221
Taiywo <54299107+Taiywo@users.noreply.github.com>
2024-03-03 19:21:22 +0100

    add more info

--- src/components/data/Data.jsx
+++ src/components/data/Data.jsx
@@ -182,42 +182,28 @@ export const faq = [
 export const courses = [
 {
     title:"Are you feeling trapped in a stagnant job that no longer aligns with your goals or vision for the future?",
-    content:" By the end of this training, you will be able to implement IAM solutions that control user access and privileges, reducing the risk of unauthorized data breaches and ensuring secure user authentication.",
-    icon:<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-black dark:text-white">
-    <path fillRule="evenodd" d="M15.75 1.5a6.75 6.75 0 00-6.651 7.906c.067.39-.032.717-.221.906l-6.5 6.499a3 3 0 00-.878 2.121v2.818c0 .414.336.75.75.75H6a.75.75 0 00.75-.75v-1.5h1.5A.75.75 0 009 19.5V18h1.5a.75.75 0 00.53-.22l2.658-2.658c.19-.189.517-.288.906-.22A6.75 6.75 0 1015.75 1.5zm0 3a.75.75 0 000 1.5A2.25 2.25 0 0118 8.25a.75.75 0 001.5 0 3.75 3.75 0 00-3.75-3.75z" clipRule="evenodd" />
-  </svg>
+    image:<img class="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1" />
   
 },
 {
     title:"Are you ready to skyrocket your earnings and achieve six-figure success in record time?",
-    content:" After completing this module, you will have the skills to monitor and maintain the health of systems and networks, ensuring high availability and minimizing downtime.",
-    icon:<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6  text-black dark:text-white">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
-  </svg>
+    
+    image:<img class="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1" />
   
 },
 {
     title:"Are you feeling trapped in a stagnant job that no longer aligns with your goals or vision for the future?  ",
-    content:" You will learn how to proactively apply patches and manage vulnerabilities, reducing the exposure to known security risks and enhancing overall system security.",
-    icon:<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6  text-black dark:text-white">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5m.75-9l3-3 2.148 2.148A12.061 12.061 0 0116.5 7.605" />
-  </svg>
+   image:<img class="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1" />
   
 },
 {
     title:"Are you eager to kickstart a career in a dynamic field that offers diverse career trajectories and endless possibilities for professional advancement? ",
-    content:" You will learn how to proactively apply patches and manage vulnerabilities, reducing the exposure to known security risks and enhancing overall system security.",
-    icon:<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6  text-black dark:text-white">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5m.75-9l3-3 2.148 2.148A12.061 12.061 0 0116.5 7.605" />
-  </svg>
+   image:<img class="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1" />
   
 },
 {
     title:"Ready to make a bold career move even without a degree or prior experience in IT?  ",
-    content:" You will learn how to proactively apply patches and manage vulnerabilities, reducing the exposure to known security risks and enhancing overall system security.",
-    icon:<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6  text-black dark:text-white">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3v11.25A2.25 2.25 0 006 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0118 16.5h-2.25m-7.5 0h7.5m-7.5 0l-1 3m8.5-3l1 3m0 0l.5 1.5m-.5-1.5h-9.5m0 0l-.5 1.5m.75-9l3-3 2.148 2.148A12.061 12.061 0 0116.5 7.605" />
-  </svg>
+    image: <img class="w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/content/office-long-2.png" alt="office content 1" />
   
 },
 
--- src/components/pages/Course.jsx
+++ src/components/pages/Course.jsx
@@ -1,42 +1,94 @@
-import { Fade } from "react-awesome-reveal"
-import { courses } from "../data/Data"
-import Button from "../Button"
+import { Fade } from "react-awesome-reveal";
+import { courses } from "../data/Data";
+import Button from "../Button";
 
 export default function Course() {
   return (
-    <section className="bg-white dark:bg-gray-900 pt-14"  id="course">
+    <section className="bg-white dark:bg-gray-900 pt-14" id="course">
       <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
         <div className="max-w-screen-md mb-8 lg:mb-16">
-          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Ready to transform your life? </h2>
-         
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+            Ready to transform your life?{" "}
+          </h2>
         </div>
-        <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
-          {
-            courses.map((course, index) =>
-           <Fade>
-   <div>
-                <div key={index} className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                  
-                  {course.icon}
-                     </div>
-                <h3 className="mb-2 text-xl font-bold dark:text-white">{course.title}</h3>
-                <p className="text-gray-500 dark:text-gray-400">{course.content}</p>
+        {/* <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+          {courses.map((course, index) => (
+            <Fade>
+              <div>
+                <h3 className="mb-2 text-xl font-bold dark:text-white">
+                  {course.title}
+                </h3>
+                <div key={index}>{course.image}</div>
               </div>
+            </Fade>
+          ))}
+        </div> */}
 
-
-           </Fade>
-
-            )
-          }
-          
-        
+        <div className="md:grid lg:grid-cols-3 md:gr lg:grid-rows-4 lg:gap-12">
+          <div className="row-span-2">
+            <img
+              class="w-full rounded-lg"
+              src="https://res.cloudinary.com/dzgbjty7c/image/upload/v1709487235/Baudspace%20Cyberlearn/_72c3bdb0-33a2-46d4-be5d-185eac3ba5c9_fsdptl.jpg"
+              alt="office content 1"
+            />
+          </div>
+          <div className="row-span-3 col-start-2 row-start-2 ">
+            <img
+              class="w-full rounded-lg"
+              src="https://res.cloudinary.com/dzgbjty7c/image/upload/v1709487235/Baudspace%20Cyberlearn/_d5fd39d9-d8cf-4b5d-b82c-b879353906ca_xeqvd0.jpg"
+              alt="office content 1"
+            />
+          </div>
+          <div className="row-span-2 col-start-3 row-start-1 ">
+            <img
+              class="w-full rounded-lg"
+              src="https://res.cloudinary.com/dzgbjty7c/image/upload/v1709487234/Baudspace%20Cyberlearn/_0bfa4770-0963-4d06-832d-c6bd645af1de_oc7ris.jpg"
+              alt="office content 1"
+            />
+          </div>
+          <div className="col-start-2 row-start-1 w-full round-lg bg-blue-950">
+            <h3 className="mb-2 text-xl text-center py-6 px-2 font-bold dark:text-white">
+              Are you feeling trapped in a stagnant job that no longer aligns
+              with your goals or vision for the future?
+            </h3>
+          </div>
+          <div className="row-start-3 bg-blue-950 ">
+            <h3 className="mb-2 text-xl text-center  py-10 font-bold dark:text-white">
+              Are you ready to skyrocket your earnings and achieve six-figure
+              success in record time?
+            </h3>
+          </div>
+          <div className="col-start-3 row-start-3 bg-blue-950">
+            <h3 className="mb-2 text-xl text-center py-6 px-2 font-bold dark:text-white">
+              Are you eager to kickstart a career in a dynamic field that offers
+              diverse career trajectories and endless possibilities for
+              professional advancement?
+            </h3>
+          </div>
+          <div className="col-start-3 row-start-4 bg-blue-950">
+            <h3 className="mb-2 text-xl text-center py-6 px-2 font-bold dark:text-white">
+            Ready to make a bold career move even without a degree or prior experience in IT? 
+            </h3>
+          </div>
+          <div className="col-start-1 row-start-4 bg-blue-950">
+            <h3 className="mb-2 text-xl text-center py-6 px-2 font-bold dark:text-white">
+              Are you feeling trapped in a stagnant job that no longer aligns
+              with your goals or vision for the future?
+            </h3>
+          </div>
         </div>
-        <p className="text-gray-500 sm:text-xl pt-9 dark:text-gray-200 text-center">Join us now if you're 
-        seeking profound change and growth. Our tailored training program is designed to maximize your potential and unlock new opportunities. 
-        Don't wait any longer – take the first step towards a brighter future today</p>
+
+        <p className="text-gray-500 sm:text-xl pt-9 dark:text-gray-200 text-center">
+          Join us now if you're seeking profound change and growth. Our tailored
+          training program is designed to maximize your potential and unlock new
+          opportunities. Don't wait any longer – take the first step towards a
+          brighter future today
+        </p>
       </div>
-      <div className="flex justify-center pt-10"> <Button text="Join Us Now!" /></div>
-      
+      <div className="flex justify-center pt-10">
+        {" "}
+        <Button text="Join Us Now!" />
+      </div>
     </section>
-  )
+  );
 }
