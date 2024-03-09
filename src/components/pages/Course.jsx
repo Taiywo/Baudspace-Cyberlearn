dc7ed7434dc027275ff38f3ac031814e4e59947d
Taiywo <54299107+Taiywo@users.noreply.github.com>
2024-03-09 17:50:34 +0100

    responsiveness

--- src/components/pages/Course.jsx
+++ src/components/pages/Course.jsx
@@ -24,60 +24,60 @@ export default function Course() {
           ))}
         </div> */}
 
-        <div className="grid  md:grid-cols-2 md:gap-4 md:grid-rows-6  lg:grid-cols-3 lg:grid-rows-4 lg:gap-6  ">
-          <div className="row-span-2">
+        <div className="grid grid-row-6 grid-cols-1 gap-4   md:grid-cols-2 md:gap-4 md:grid-rows-6  lg:grid-cols-3 lg:grid-rows-4 lg:gap-6  ">
+          <div className="md:row-span-2">
             <img
               class="w-full rounded-lg"
               src="https://res.cloudinary.com/dzgbjty7c/image/upload/v1709487235/Baudspace%20Cyberlearn/_72c3bdb0-33a2-46d4-be5d-185eac3ba5c9_fsdptl.jpg"
               alt="office content 1"
             />
           </div>
-          <div className="md:row-span-4 md:col-start-2 md:row-start-3 lg:row-span-3 lg:col-start-2 lg:row-start-2 ">
+          <div className="row-start-4 md:row-span-4 md:col-start-2 md:row-start-3 lg:row-span-3 lg:col-start-2 lg:row-start-2 ">
             <img
               class="w-full rounded-lg"
               src="https://res.cloudinary.com/dzgbjty7c/image/upload/v1709487235/Baudspace%20Cyberlearn/_d5fd39d9-d8cf-4b5d-b82c-b879353906ca_xeqvd0.jpg"
               alt="office content 1"
             />
           </div>
-          <div className="md:row-span-6 md:col-start-1 md:row-start-5 lg:row-span-2 lg:col-start-3 lg:row-start-1 ">
+          <div className="row-start-7 md:row-span-6 md:col-start-1 md:row-start-5 lg:row-span-2 lg:col-start-3 lg:row-start-1 ">
             <img
               class="w-full rounded-lg"
               src="https://res.cloudinary.com/dzgbjty7c/image/upload/v1709487234/Baudspace%20Cyberlearn/_0bfa4770-0963-4d06-832d-c6bd645af1de_oc7ris.jpg"
               alt="office content 1"
             />
           </div>
-          <div className=" md:col-start-2 md:row-start-1 lg:row-span-2 lg:col-start-2 lg:row-start-1 w-full round-lg bg-blue-950">
+          <div className=" row-start-2 md:col-start-2 md:row-start-1 lg:row-span-2 lg:col-start-2 lg:row-start-1 w-full round-lg bg-blue-950">
             <h3 className="mb-2 text-xl text-center py-6 px-2 font-bold text-white">
               Feeling trapped in a stagnant job that no longer aligns with your
               goals or vision for the future?
             </h3>
           </div>
-          <div className="md:col-start-2 md:row-start-2 lg:row-start-3 bg-blue-950 ">
+          <div className="row-start-3 md:col-start-2 md:row-start-2 lg:row-start-3 bg-blue-950 ">
             <h3 className="mb-2 text-xl text-center  py-10 font-bold text-white">
               Ready to skyrocket your earnings and achieve six-figure success in
               record time?
             </h3>
           </div>
-          <div className="md:col-start-1 md:row-start-3 lg:col-start-3 lg:row-start-3 bg-blue-950">
+          <div className="row-start-5 md:col-start-1 md:row-start-3 lg:col-start-3 lg:row-start-3 bg-blue-950">
             <h3 className="mb-2 text-xl text-center py-6 px-2 font-bold text-white">
               Eager to kickstart a career in a dynamic field that offers diverse
               career trajectories and endless possibilities for professional
               advancement?
             </h3>
           </div>
-          <div className="md:col-start-2 md:row-start-4 lg:col-start-3 lg:row-start-4 bg-blue-950">
+          <div className="row-start-6 md:col-start-1 md:row-start-4 lg:col-start-3 lg:row-start-4 bg-blue-950">
             <h3 className="mb-2 text-xl text-center py-6 px-2 font-bold text-white">
               Ready to make a bold career move even without a degree or prior
               experience in IT?
             </h3>
           </div>
-          <div className="md:col-start-2 md:row-start-5 lg:col-start-1 lg:row-start-4 bg-blue-950">
+          <div className="row-start-8 md:col-start-2 md:row-start-5 lg:col-start-1 lg:row-start-4 bg-blue-950">
             <h3 className="mb-2 text-xl text-center py-6 px-2 font-bold text-white">
               Feeling trapped in a stagnant job that no longer aligns with your
               goals or vision for the future?
             </h3>
           </div>
-          <div className="md:col-start-2 md:row-start-6 lg:col-start-2 lg:row-start-4 bg-blue-950">
+          <div className="row-start-9 md:col-start-2 md:row-start-6 lg:col-start-2 lg:row-start-4 bg-blue-950">
             <h3 className="mb-2 text-xl text-center m-auto py-6 px-2 font-bold text-white">
               Simply tired of earning below your potential.
             </h3>
